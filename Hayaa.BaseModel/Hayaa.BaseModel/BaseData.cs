0ad3205872ce0437986685c364e187958c9f3b45
hsieh jing <xqj198404@gmail.com>
2017-12-19 15:20:58 +0800

    增加BaseData两个属性:CreateTime、UpdateTime

--- Hayaa.BaseModel/Hayaa.BaseModel/BaseData.cs
+++ Hayaa.BaseModel/Hayaa.BaseModel/BaseData.cs
@@ -18,5 +18,13 @@ namespace Hayaa.BaseModel
         /// 业务返回提示信息
         /// </summary>
         public string Message { set; get; }
+        /// <summary>
+        /// 数据创建时间
+        /// </summary>
+        public DateTime CreateTime { set; get; }
+        /// <summary>
+        /// 数据更新时间
+        /// </summary>
+        public DateTime UpdateTime { set; get; }
     }
 }
