28f4e7e1bc25fb6be904ac36e2e8a9bb20c1ce60
hsieh jing <xqj198404@gmail.com>
2017-12-19 17:03:26 +0800

    增加分页参数类型

---
+++ Hayaa.BaseModel/Hayaa.BaseModel/GroupBy.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hayaa.BaseModel
+{
+    /// <summary>
+    /// 分组元数据
+    /// </summary>
+  public  class GroupBy
+    {
+        /// <summary>
+        /// 分组名称
+        /// </summary>
+        public String GroupByName { set; get; }
+        /// <summary>
+        /// 排序优先级别，小数字靠前，相同数字则随机
+        /// </summary>
+        public int SortIndex { set; get; }
+    }
+}
---
+++ Hayaa.BaseModel/Hayaa.BaseModel/OrderBy.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hayaa.BaseModel
+{
+    /// <summary>
+    /// 排序元数据
+    /// </summary>
+   public class OrderBy
+    {
+        /// <summary>
+        /// 数据要排序属性名称
+        /// </summary>
+        public String OrderByName { set; get; }
+        /// <summary>
+        /// 排序类型
+        /// </summary>
+        public OrderDirection OrderByDirection { set; get; }
+        /// <summary>
+        /// 排序优先级别，小数字靠前，相同数字则随机
+        /// </summary>
+        public int SortIndex { set; get; }
+    }
+}
