cdfe6b7b3a637afad9b793fcc4c51a1b885b08be
hsieh jing <xqj198404@gmail.com>
2018-01-05 17:22:02 +0800

    修改检查规则逻辑判断结构

--- Hayaa.BaseModel/Hayaa.Common/CheckHelper.cs
+++ Hayaa.BaseModel/Hayaa.Common/CheckHelper.cs
@@ -6,35 +6,35 @@ namespace Hayaa.Common
 {
     public class CheckHelper
     {
-        public static bool AssertStringNullorEmpty(String target) 
+        public static bool IsStringNullorEmpty(String target) 
         {
             if (String.IsNullOrWhiteSpace(target))
             {
-                return false;
+                return true;
             }
-            return true;
+            return false;
         }
-        public static bool AssertListNullorEmpty<T>(List<T> target)
+        public static bool IsListNullorEmpty<T>(List<T> target)
         {
             if (target==null)
             {
-                return false;
+                return true;
             }
             if (target.Count==0)
             {
-                return false;
+                return true;
             }
-            return true;
+            return false;
         }
-        public static bool AssertNull<T>(T target) where T:class
+        public static bool IsNull<T>(T target) where T:class
         {
             if (target == null)
             {
-                return false;
+                return true;
             }
-            return true;
+            return false;
         }
-        public static bool AssertRangByte(byte target, byte min, byte max)
+        public static bool IsRangByte(byte target, byte min, byte max)
         {
             if ((target < min) || (target > max))
             {
@@ -42,7 +42,7 @@ namespace Hayaa.Common
             }
             return true;
         }
-        public static bool AssertRangInt(int target,int min,int max) 
+        public static bool IsRangInt(int target,int min,int max) 
         {
             if ((target < min) || (target > max))
             {
@@ -50,7 +50,7 @@ namespace Hayaa.Common
             }
             return true;
         }
-        public static bool AssertRangLong(long target, long min, long max)
+        public static bool IsRangLong(long target, long min, long max)
         {
             if ((target < min) || (target > max))
             {
@@ -58,7 +58,7 @@ namespace Hayaa.Common
             }
             return true;
         }
-        public static bool AssertRangDecimal(decimal target, decimal min, decimal max)
+        public static bool IsRangDecimal(decimal target, decimal min, decimal max)
         {
             if ((target < min) || (target > max))
             {
@@ -66,7 +66,7 @@ namespace Hayaa.Common
             }
             return true;
         }
-        public static bool AssertRangDouble(double target, double min, double max)
+        public static bool IsRangDouble(double target, double min, double max)
         {
             if ((target < min) || (target > max))
             {
@@ -74,7 +74,7 @@ namespace Hayaa.Common
             }
             return true;
         }
-        public static bool AssertRangFloat(float target, float min, float max)
+        public static bool IsRangFloat(float target, float min, float max)
         {
             if ((target < min) || (target > max))
             {
@@ -82,7 +82,7 @@ namespace Hayaa.Common
             }
             return true;
         }
-        public static bool AssertRangDateTime(DateTime target, DateTime min, DateTime max)
+        public static bool IsRangDateTime(DateTime target, DateTime min, DateTime max)
         {
             if ((target < min) || (target > max))
             {
@@ -91,33 +91,25 @@ namespace Hayaa.Common
             return true;
         }
         private static readonly Regex MobileRegex = new Regex(RegexRuleHelper.Mobile, RegexOptions.Compiled);
-        public static bool AssertRegexMobile(string target)
+        public static bool IsRegexMobile(string target)
         {
             if (string.IsNullOrWhiteSpace(target))
             {
                 return false;
             }
             target = target.Trim();
-            if (!MobileRegex.IsMatch(target))
-            {
-                return false;
-            }
-            return true;
+            return MobileRegex.IsMatch(target);
         }
         private static readonly Regex EmailRegex = new Regex(RegexRuleHelper.Email,
             RegexOptions.Compiled);
-        public static bool AssertRegexEmail(string target)
+        public static bool IsRegexEmail(string target)
         {
             if (string.IsNullOrWhiteSpace(target))
             {
                 return false;
             }
             target = target.Trim();
-            if (!EmailRegex.IsMatch(target))
-            {
-                return false;
-            }
-            return true;
+            return EmailRegex.IsMatch(target);
         }
         private static readonly Regex UrlRegex = new Regex(RegexRuleHelper.HttpUrl,
           RegexOptions.Compiled);
@@ -125,86 +117,68 @@ namespace Hayaa.Common
         /// 断言含有http的url地址
         /// </summary>
         /// <param name="target"></param>
-        public static bool AssertRegexUrl(string target)
+        public static bool IsRegexUrl(string target)
         {
             if (string.IsNullOrWhiteSpace(target))
             {
                 return false;
             }
             target = target.Trim();
-            if (!UrlRegex.IsMatch(target))
-            {
-                return false;
-            }
-            return true;
+            
+            return UrlRegex.IsMatch(target);
         }
         private static readonly Regex IDCardRegex = new Regex(RegexRuleHelper.IDCard, RegexOptions.Compiled);
         /// <summary>
         /// 是否是国内身份证号
         /// </summary>
          /// <returns></returns>
-        public static bool AssertIDCard(string target)
+        public static bool IsIDCard(string target)
         {
             if (string.IsNullOrWhiteSpace(target))
             {
                 return false;
             }
             target = target.Trim();
-            if (!IDCardRegex.IsMatch(target))
-            {
-                return false;
-            }
-            return true;
+            
+            return IDCardRegex.IsMatch(target);
         }
         private static readonly Regex EnglishRegex = new Regex(RegexRuleHelper.English, RegexOptions.Compiled);
         /// <summary>
         /// 是否英文
         /// </summary>
         /// <returns></returns>
-        public static bool AssertEnglish(string target)
+        public static bool IsEnglish(string target)
         {
             if (string.IsNullOrWhiteSpace(target))
             {
                 return false;
             }
-            target = target.Trim();
-            if (!EnglishRegex.IsMatch(target))
-            {
-                return false;
-            }
-            return true;
+            target = target.Trim();            
+            return EnglishRegex.IsMatch(target);
         }
         private static readonly Regex ChineseRegex = new Regex(RegexRuleHelper.Chinese, RegexOptions.Compiled);
         /// <summary>
         /// 是否中文
         /// </summary>
         /// <returns></returns>
-        public static bool AssertChinese(string target)
+        public static bool IsChinese(string target)
         {
             if (string.IsNullOrWhiteSpace(target))
             {
                 return false;
             }
-            target = target.Trim();
-            if (!ChineseRegex.IsMatch(target))
-            {
-                return false;
-            }
-            return true;
+            target = target.Trim();            
+            return ChineseRegex.IsMatch(target);
         }
-        public static bool AssertRegex(string target, String regexRule)
+        public static bool IsRegex(string target, String regexRule)
         {
             if (string.IsNullOrWhiteSpace(target))
             {
                 return false;
             }
             target = target.Trim();
-            Regex regex = new Regex(regexRule, RegexOptions.Compiled);
-            if (!regex.IsMatch(target))
-            {
-                return false;
-            }
-            return true;
+            Regex regex = new Regex(regexRule, RegexOptions.Compiled);            
+            return regex.IsMatch(target);
         }
     }
 }
