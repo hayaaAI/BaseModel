998738aca2aea5ce73f59a999d9873a4813706f1
hayaa <dev@hayaa-ai.com>
2018-01-13 18:16:46 +0800

    增加HttpContext类

---
+++ Hayaa.BaseModel/Hayaa.Common/HttpContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hayaa.Common
+{
+    public static class HttpContext
+    {
+        public static IServiceProvider ServiceProvider;
+
+        public static Microsoft.AspNetCore.Http.HttpContext Current
+        {
+            get
+            {
+                object factory = ServiceProvider.GetService(typeof(IHttpContextAccessor));
+                Microsoft.AspNetCore.Http.HttpContext context = ((HttpContextAccessor)factory).HttpContext;
+                return context;
+            }
+        }
+
+    }
+}
