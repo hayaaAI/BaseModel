abd86b9e5527e370fce9e845830602014fe59aa7
hsieh jing <xqj198404@gmail.com>
2018-01-15 15:36:57 +0800

    添加远程配置组件内容约束

---
+++ Hayaa.BaseModel/Hayaa.BaseModel/Model/ConfigContent.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hayaa.BaseModel
+{
+    /// <summary>
+    /// 远程配置内容约束
+    /// </summary>
+    public interface ConfigContent
+    {
+         AppSettings appSettings { set; get; }
+
+         ConnectionStrings connectionStrings { set; get; }
+    }
+    [Serializable]
+    public class AppSettings
+    {
+
+        public List<AppSetInfo> Settings;
+
+        public class AppSetInfo
+        {
+
+            public string Key { set; get; }
+
+            public string Value { set; get; }
+        }
+    }
+    [Serializable]
+    public class ConnectionStrings
+    {
+        public List<ConnectionInfo> Settings;
+
+        public class ConnectionInfo
+        {
+
+            public string Key { set; get; }
+
+            public string Connection { set; get; }
+        }
+
+
+    }
+}
