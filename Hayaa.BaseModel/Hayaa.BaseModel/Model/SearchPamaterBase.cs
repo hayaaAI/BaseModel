4d9df2ad3a868f57e5b456ac697503c0791becff
hayaa <dev@hayaa-ai.com>
2018-04-03 13:07:32 +0800

    增加基于Mariadb的参数基础类

---
+++ Hayaa.BaseModel/Hayaa.BaseModel/Model/SearchPamaterBase.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Linq;
+
+namespace Hayaa.BaseModel
+{
+    /// <summary>
+    /// 查询参数基础类
+    /// </summary>
+  public abstract  class SearchPamaterMariadbBase
+    {
+      
+      
+        public String CreateWhereSql()
+        {           
+            Type type = this.GetType(); 
+            List<PropertyInfo> listPinfo = type.GetProperties().ToList();
+            List<MethodInfo> methods=type.GetMethods().ToList();
+            List<String> list = new List<string>();
+            listPinfo.ForEach(pi => {
+                String methodName = String.Format("Get{0}Sql",pi.Name);
+                MethodInfo method = methods.Find(a => a.Name == methodName);
+                list.Add(method.Invoke(type, null).ToString());
+            });
+            if (list.Count > 0)
+                return String.Format("where {0}", String.Join(" and ", list));
+            else
+                return "";
+        }
+       
+    }
+}
--- Hayaa.BaseModel/Hayaa.DataAccess/CommonDal.cs
+++ Hayaa.BaseModel/Hayaa.DataAccess/CommonDal.cs
@@ -4,6 +4,8 @@ using System.Data;
 using Dapper;
 using System.Collections.Generic;
 using Hayaa.BaseModel;
+using System.Text;
+using System.Reflection;
 
 namespace Hayaa.DataAccess
 {
@@ -14,27 +16,27 @@ namespace Hayaa.DataAccess
             IDbConnection conn = new MySqlConnection(conectionString);
             return conn.Execute(sql, data);
         }
-        protected static int Update<T>(String conectionString,string sql, T data)
+        protected static int Update<T>(String conectionString, string sql, T data)
         {
             IDbConnection conn = new MySqlConnection(conectionString);
             return conn.Execute(sql, data);
         }
-        protected static int Excute(String conectionString,string sql, object parama = null)
+        protected static int Excute(String conectionString, string sql, object parama = null)
         {
             IDbConnection conn = new MySqlConnection(conectionString);
             return conn.Execute(sql, parama);
         }
-        protected static List<T> GetList<T>(String conectionString,string sql, object parama)
+        protected static List<T> GetList<T>(String conectionString, string sql, object parama)
         {
             IDbConnection conn = new MySqlConnection(conectionString);
             return conn.Query<T>(sql, parama).AsList<T>();
         }
-        protected static T Get<T>(String conectionString,string sql, object parama)
+        protected static T Get<T>(String conectionString, string sql, object parama)
         {
             IDbConnection conn = new MySqlConnection(conectionString);
             return conn.QuerySingle<T>(sql, parama);
         }
-        protected static GridPager<T> GetGridPager<T>(String conectionString,string sql, int pageSize, int pageIndex, object parama) where T: BaseData
+        protected static GridPager<T> GetGridPager<T>(String conectionString, string sql, int pageSize, int pageIndex, object parama) where T : BaseData
         {
             GridPager<T> r = new GridPager<T>() { CurrentIndex = pageIndex, PageSize = pageSize, ActionResult = false };
             IDbConnection conn = new MySqlConnection(conectionString);
@@ -42,9 +44,10 @@ namespace Hayaa.DataAccess
             if (!multi.IsConsumed)
             {
                 r.Data = multi.Read<T>().AsList<T>();
-                r.Total = multi.ReadSingle();              
+                r.Total = multi.ReadSingle();
             }
             return r;
         }
+      
     }
 }
