a80b7a47de3c5e850903508a0b87ce3ce4d1c9ef
hayaa <dev@hayaa-ai.com>
2018-04-18 00:34:56 +0800

    增加传输结果容器类

---
+++ Hayaa.BaseModel/Hayaa.BaseModel/Model/TransactionResult.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hayaa.BaseModel.Model
+{
+    /// <summary>
+    /// 传输结果容器
+    /// </summary>
+    public class TransactionResult<T>
+    {
+        /// <summary>
+        /// 返回码
+        /// </summary>
+        public int Code { set; get; }
+        /// <summary>
+        /// 提示信息、异常信息
+        /// </summary>
+        public String Message { set; get; }
+        /// <summary>
+        /// 数据结果
+        /// </summary>
+        public T Data { set; get; }
+    }
+}
