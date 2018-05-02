9b3a7129d9d78bde5737024f2acb8d5d87b54ecd
hayaa <dev@hayaa-ai.com>
2018-05-02 15:42:22 +0800

    修复http服务端请求产生空参数的问题

--- Hayaa.BaseModel/Hayaa.Common/HttpHelper.cs
+++ Hayaa.BaseModel/Hayaa.Common/HttpHelper.cs
@@ -23,7 +23,7 @@ namespace Hayaa.Common
                 requestUrl = string.Format("http://{0}", requestUrl);
             HttpWebRequest req = (HttpWebRequest)HttpWebRequest.Create(requestUrl);
             Encoding encoding = Encoding.UTF8;
-            StringBuilder param = new StringBuilder("&");
+            StringBuilder param = new StringBuilder("");
             if (urlParam != null)
             {
                 foreach (var kv in urlParam)
