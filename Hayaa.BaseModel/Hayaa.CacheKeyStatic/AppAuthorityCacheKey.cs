c11b0cd23d4765459f4350a872ab0d767c237e78
hayaa <dev@hayaa-ai.com>
2018-06-07 03:05:33 +0800

    增加缓存字符常量

---
+++ Hayaa.BaseModel/Hayaa.CacheKeyStatic/AppAuthorityCacheKey.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hayaa.CacheKeyStatic
+{
+  public  class AppAuthorityCacheKeys
+    {
+        /// <summary>
+        /// 实例配置key,以实例ID作为变量
+        /// </summary>
+        public const String AppAuthorityCacheKey = "appauth_token_{0}";
+    }
+}
---
+++ Hayaa.BaseModel/Hayaa.CacheKeyStatic/ConfigAuthorityCacheKey.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hayaa.CacheKeyStatic
+{
+   public class ConfigAuthorityCacheKey
+    {
+        /// <summary>
+        /// 远程配置key，以AppId做为变量
+        /// </summary>
+        public const String AuthorityCacheKey = "configauth_token_{0}";
+        
+    }
+}
