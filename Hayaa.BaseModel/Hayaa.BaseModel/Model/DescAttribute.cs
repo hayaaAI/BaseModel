2ec9c8351dceb240e2b08452dba603024af4570f
hayaa <dev@hayaa-ai.com>
2018-06-10 19:06:46 +0800

    增加描述属性类

---
+++ Hayaa.BaseModel/Hayaa.BaseModel/Model/DescAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hayaa.BaseModel.Model
+{
+    [AttributeUsage(AttributeTargets.Class| AttributeTargets.Method)]
+    public sealed class DescAttribute: Attribute
+    {
+        public DescAttribute(String name,String title,String remark)
+        {
+            this.Name = name;
+            this.Title = title;
+            this.Remark = remark;
+        }
+        public String Name { private set; get; }
+        public String Title { private set; get; }
+        public String Remark { private set; get; }
+    }
+}
