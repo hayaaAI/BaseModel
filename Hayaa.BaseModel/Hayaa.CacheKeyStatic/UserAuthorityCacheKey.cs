9becf51f19d009d10bd921420a7052e32d2d01ad
hayaa <dev@hayaa-ai.com>
2018-06-22 11:50:33 +0800

    添加用户登陆的缓存key

---
+++ Hayaa.BaseModel/Hayaa.CacheKeyStatic/UserAuthorityCacheKey.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hayaa.CacheKeyStatic
+{
+   public class UserAuthorityCacheKey
+    {
+        public const String AuthorityCacheKey = "userauth_token_{0}";
+    }
+}
