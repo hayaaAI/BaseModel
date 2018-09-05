57cb023166491be4d738a82220a845d0416213d7
hayaa <dev@hayaa-ai.com>
2018-09-05 17:59:22 +0800

    添加通用异常码

---
+++ Hayaa.BaseModel/Hayaa.Common/ErrorCode.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hayaa.Common
+{
+    /// <summary>
+    /// 错误码集合
+    /// </summary>
+   public class ErrorCode
+    {
+        /// <summary>
+        /// 登陆失败
+        /// </summary>
+        public const int LoginFail = 1001;
+        /// <summary>
+        /// 密码破解攻击
+        /// </summary>
+        public const int PwdAttack = 1002;
+    }
+}
