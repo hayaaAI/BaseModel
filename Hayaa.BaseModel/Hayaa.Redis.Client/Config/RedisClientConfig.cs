18d86c2bd147a7a15aa7337b051aeb6d470cc7d4
hayaa <dev@hayaa-ai.com>
2018-09-05 13:08:46 +0800

    去点redis客户端的远程配置组件，并将项目迁移到Common方案中

---
+++ Hayaa.BaseModel/Hayaa.Redis.Client/Config/RedisClientConfig.cs
@@ -0,0 +1,18 @@
+﻿using Hayaa.Redis.Client.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hayaa.Redis.Client.Config
+{
+    /// <summary>
+    /// 服务配置类
+    /// </summary>
+    [Serializable]
+    public class RedisClientConfig 
+    {
+        public List<RedisConfig> RedisConfigs { set; get;  }
+      
+
+    }
+}
---
+++ Hayaa.BaseModel/Hayaa.Redis.Client/RedisService.cs
@@ -0,0 +1,59 @@
+﻿using Hayaa.Common;
+using Hayaa.Redis.Client.Config;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hayaa.Redis.Client
+{
+   public class RedisService
+    {
+        private RedisFactory redisFactory = null;
+        public RedisService(RedisClientConfig config)
+        {
+            redisFactory = new RedisFactory(config);
+        }
+        public  void Set<T>(String name, String key, T data) where T:class
+        {
+            var redis = redisFactory.GetService(name);
+            if (redis != null)
+            {
+                redis.StringSet(key, JsonHelper.SerlaizeSafe<T>(data));
+            }
+        }
+       
+        public  T Get<T>(String name, String key) where T : class
+        {
+            T data = default(T);
+            var redis = redisFactory.GetService(name);
+            if (redis != null)
+            {
+              String redisResult=  redis.StringGet(key);
+                if (!String.IsNullOrEmpty(redisResult))
+                {
+                    data = JsonHelper.Deserialize<T>(redisResult);
+                }
+            }
+            return data;
+        }
+      
+
+        public  void Set<T>(String name, String key, T data,int cacheSeconds) where T : class
+        {
+            var redis = redisFactory.GetService(name);
+            if (redis != null)
+            {
+                redis.StringSet(key, JsonHelper.SerlaizeSafe<T>(data),new TimeSpan(0,0, cacheSeconds));
+            }
+        }
+        public  void Delete(String name, String key)
+        {
+          
+            var redis = redisFactory.GetService(name);
+            if (redis != null)
+            {
+                redis.KeyDelete(key);
+            }
+        }
+    }
+}
