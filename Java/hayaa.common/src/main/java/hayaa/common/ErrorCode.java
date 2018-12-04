ce83aa1e073fd3acdaa2fc4f09dc46130346be7d
hayaa <dev@hayaa-ai.com>
2018-12-04 12:20:58 +0800

    添加错误代码

---
+++ Java/hayaa.common/src/main/java/hayaa/common/ErrorCode.java
@@ -0,0 +1,24 @@
+package hayaa.common;
+
+/**
+ * @author hsieh
+ * label
+ */
+public class ErrorCode {
+    /**
+     * 登陆失败
+     */
+    public final static int LOGIN_FAIL = 1001;
+    /**
+     * 密码破解攻击
+     */
+    public final static int PWD_ATTACK = 1002;
+    /**
+     * 无授权
+     */
+    public final static int NO_PERMISSION = 1003;
+    /**
+     * 无数据
+     */
+    public final static int NO_DATA = 1004;
+}
